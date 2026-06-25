d42a56834ff555c00b0f98fbec0f026334e58def
Shubh Gupta <shubh.gupta@metasquaretech.com>
2026-06-25 11:34:37 +0530

    version 5

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const express = require("express");
 var cors = require("cors");
 connectToMongo();
 const app = express();
-const port = 5000;
+// const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
@@ -15,6 +16,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, () => {
-  console.log(`iNotebook backend listening at http:localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`iNotebook backend listening at http:localhost:${PORT}`);
 });
--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -6,6 +6,7 @@ const fetchuser = (req, res, next) => {
   //Get the user from the jwt token and add id to req object
   const token = req.header("auth-token");
   if (!token) {
+    return res;
     res.status(401).send({ error: "Please authenticate using a valid token" });
   }
   try {
